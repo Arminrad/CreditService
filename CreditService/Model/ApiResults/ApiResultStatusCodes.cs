344d84bfbdf3a644a9e3560d182133c5340ffaa5
Mhmdrzo <m.khani2025@gmail.com>
2022-09-16 18:12:58 +0430

    api results structure added

---
+++ CreditService/Model/ApiResults/ApiResultStatusCodes.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace CreditService.Model.ApiResults
+{
+    public enum ApiResultStatusCode
+    {
+        [Display(Description = "Account created for user")]
+        Created = 0,
+
+        [Display(Description = "The transaction was completed successfully")]
+        Success = 1,
+
+        [Display(Description = "The account balance is insufficient")]
+        Insufficient = 2,
+
+    }
+}
