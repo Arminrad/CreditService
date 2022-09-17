c79f46f749925f36e0a38511ee628cb28882d28c
pouriya <pooriya.shahbeyki@gmail.com>
2022-09-17 22:45:50 +0430

    repoCRUDcoding

--- CreditService/Repository/TransactionRecordsRepository.cs
+++ CreditService/Repository/TransactionRecordsRepository.cs
@@ -3,30 +3,55 @@
 namespace CreditService.Repository
 {
     public class TransactionRecordsRepository : GenericRepository<TransactionRecords, int>
-    {
+    { 
+        private readonly CreditContext _creditContext;
+        public AccountRepository(CreditContext creditContext)
+        {
+            _creditContext = creditContext;       
+
+        }
         public int Delete(TransactionRecords t)
         {
-            throw new NotImplementedException();
+        
+        var data = _creditContext.TransactionRecords.FirstOrDefault(x => x.Id == t.Id);
+            if (data != null)
+            {
+                _creditContext.Accounts.Remove(data);
+                _creditContext.SaveChangesAsync();
+                return 1;
+            }
+            return 0;
         }
-
         public List<TransactionRecords> GetAll()
         {
-            throw new NotImplementedException();
+            return _creditContext.TransactionRecords.ToList();
         }
 
         public TransactionRecords GetById(int id)
         {
-            throw new NotImplementedException();
+            return _creditContext.TransactionRecords.SingleOrDefault(x => x.Id == id);
         }
 
-        public int Insert(TransactionRecords t)
+        public async Task Insert(TransactionRecords t)
         {
-            throw new NotImplementedException();
+            await _creditContext.TransactionRecords.AddAsync(t);
+            _creditContext.SaveChangesAsync();
         }
 
         public int Update(TransactionRecords t)
         {
-            throw new NotImplementedException();
+            var data = _creditContext.TransactionRecords.SingleOrDefault(x => x.Id == t.Id);
+            if (data != null)
+            {
+                data.UserId = t.UserId;
+                data.Amount = t.Amount;
+                data.IsDeposit = t.IsDeposit;
+                data.TransactionTime = t.TransactionTime;
+
+                _creditContext.SaveChangesAsync();
+                return 1;
+            }
+            return 0;
         }
     }
 }
--- CreditService/Repository/TransactionRepository.cs
+++ CreditService/Repository/TransactionRepository.cs
@@ -4,29 +4,53 @@ namespace CreditService.Repository
 {
     public class TransactionRepository : GenericRepository<Transaction, int>
     {
+        private readonly CreditContext _creditContext;
+        public AccountRepository(CreditContext creditContext)
+        {
+            _creditContext = creditContext;
+        }
+
         public int Delete(Transaction t)
         {
-            throw new NotImplementedException();
+            var data = _creditContext.Transactions.FirstOrDefault(x => x.Id == t.Id);
+            if (data != null)
+            {
+                _creditContext.Transactions.Remove(data);
+                _creditContext.SaveChangesAsync();
+                return 1;
+            }
+            return 0;
         }
 
         public List<Transaction> GetAll()
         {
-            throw new NotImplementedException();
+            return _creditContext.Transactions.ToList();
         }
 
         public Transaction GetById(int id)
         {
-            throw new NotImplementedException();
+            return _creditContext.Transactions.SingleOrDefault(x => x.Id == id);
         }
 
-        public int Insert(Transaction t)
+        public async Task Insert(Transaction t)
         {
-            throw new NotImplementedException();
+            await _creditContext.Transactions.AddAsync(t);
+            _creditContext.SaveChangesAsync();
         }
 
         public int Update(Transaction t)
         {
-            throw new NotImplementedException();
+            var data = _creditContext.Transactions.SingleOrDefault(x => x.Id == t.Id);
+            if (data != null)
+            {
+                data.CustomerId = t.CustomerId;
+                data.EmployerAccountId = t.EmployerAccountId;
+                data.TransactionDate = t.TransactionDate;            
+
+                _creditContext.SaveChangesAsync();
+                return 1;
+            }
+            return 0;
         }
     }
 }
