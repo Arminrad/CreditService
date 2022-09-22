8f4e954f244e7279d7b2a4b299bb86a6d0fffaa3
Mhmdrzo <m.khani2025@gmail.com>
2022-09-22 16:12:00 +0330

    fixing build errors

--- Model/CallerId.cs
+++ Model/CallerId.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.SignalR;
-using Model.Base;
+﻿using Model.Base.Implementations;
 
 namespace Model
 {
-    public class Caller: BaseEntity<Guid>
+    public class Caller : BaseEntity<Guid>
     {
         public string CallerName { get; set; }
     }
--- Repository/Connection/CreditContext.cs
+++ Repository/Connection/CreditContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Model;
 using Model.Entities;
 
 namespace Repository.Connection
